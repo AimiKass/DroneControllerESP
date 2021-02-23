bf02543c2f83708c67a5952eed5da61b581689d3
AimilianosKass <aimilianoskas@gmail.com>
2021-02-23 18:07:40 +0200

    ESP8266 understands external signal through Interrupts
    Rearrange the pins in order to fit 5 outputs for Flight Controller and 4 inputs for the external signals of receiver .
    
    The external signals are monitor by 4 interrupt methods , one for each pin/channel .
    
    The main purpose of this branch is to give priority to the receiver over signals that are send from the computer . So every time one of the 4 channels of the receiver is changed the program "disconnects" the computer's data from the specific channel for 1.5 seconds

--- ESP8266_controll_drone_through_WiFi/ESP8266_controll_drone_through_WiFi.ino
+++ ESP8266_controll_drone_through_WiFi/ESP8266_controll_drone_through_WiFi.ino
@@ -2,38 +2,58 @@
 #include <Servo.h>
 
 
-const char* ssid="LoCoMoCo";
-const char* password="patata97";
+const char* ssid = "LoCoMoCo";
+const char* password = "patata97";
 const int port = 1234;
 
 WiFiServer wifiServer(port);
 
+
+
+unsigned long previousMillis = 0;
+
+const long interval = 1500;
+
 //==========================
 
-#define CH1_Input 16 //
-#define CH2_Input 5 //
-#define CH3_Input 4 //
-#define CH4_Input 0 //
+//Connect with FlightController
+#define CH1_Output 2 //ROLL_FROM_PC
+#define CH2_Output 12 //PITCH_FROM_PC
+#define CH3_Output 13 //THROTTLE_FROM_PC
+#define CH4_Output 15 //YAW_FROM_PC
+#define CH5_Output 16 //ARM_FROM_PC
 
-#define SWC_PIN 10 // pin 10 works only as an input
+//TODO find a pin to set the arm function
+//Concect with receiver
+#define CH1_Input 5 //ROLL_FROM_RECEIVER
+#define CH2_Input 14 //PITCH_FROM_RECEIVER
+#define CH3_Input 4 //THROTTLE_FROM_RECEIVER
+#define CH4_Input 0 //YAW_FROM_RECEIVER
 
-#define CH1_Output 14
-#define CH2_Output 12
-#define CH3_Output 13
-#define CH4_Output 15
-#define CH5_Output 2
+Servo servo_1;
+Servo servo_2;
+Servo servo_3;
+Servo servo_4;
+Servo servo_5;
 
+int static interuptSensitivity = 4;
 
-Servo servo_1; //ROLL
-Servo servo_2; //PITCH
-Servo servo_3; //THROTTLE
-Servo servo_4; //YAW
-Servo servo_5; //ARM
+int CH1_Receiver_Old = 0;
+int CH2_Receiver_Old = 0;
+int CH3_Receiver_Old = 0;
+int CH4_Receiver_Old = 0;
+
+bool CH1_Interrupts_Is = false;
+bool CH2_Interrupts_Is = false;
+bool CH3_Interrupts_Is = false;
+bool CH4_Interrupts_Is = false;
+
 //==========================
 
 
-void setup() 
+void setup()
 {
+
   //==========================
   servo_1.attach(CH1_Output);
   servo_2.attach(CH2_Output);
@@ -41,99 +61,176 @@ void setup()
   servo_4.attach(CH4_Output);
   servo_5.attach(CH5_Output);
 
-  
-  pinMode(CH1_Input,INPUT);
-  pinMode(CH2_Input,INPUT);
-  pinMode(CH3_Input,INPUT);
-  pinMode(CH4_Input,INPUT);
+  pinMode(CH1_Input, INPUT_PULLUP);
+  pinMode(CH2_Input, INPUT_PULLUP);
+  pinMode(CH3_Input, INPUT_PULLUP);
+  pinMode(CH4_Input, INPUT_PULLUP);
 
-  pinMode(SWC_PIN,INPUT); 
+  attachInterrupt(digitalPinToInterrupt(CH1_Input), CH1_INTERRUPT, RISING);
+  attachInterrupt(digitalPinToInterrupt(CH2_Input), CH2_INTERRUPT, RISING);
+  attachInterrupt(digitalPinToInterrupt(CH3_Input), CH3_INTERRUPT, RISING);
+  attachInterrupt(digitalPinToInterrupt(CH4_Input), CH4_INTERRUPT, RISING);
+
   //==========================
-  
+
   Serial.begin(115200);
   Serial.println();
   Serial.println("Wifi connecting to ");
   Serial.println( ssid );
 
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid,password);
+  WiFi.begin(ssid, password);
 
   Serial.println();
   Serial.println("Connecting");
 
-  while(WiFi.status() != WL_CONNECTED )
+  while (WiFi.status() != WL_CONNECTED )
   {
-    digitalWrite(2,HIGH); //del
     delay(500);
     Serial.println(".");
-    digitalWrite(2,LOW); //del
-    delay(500);
   }
 
   Serial.println(WiFi.localIP());
-  digitalWrite(2,HIGH); //del
   wifiServer.begin();
 }
 
 
-
-void loop() 
+void loop()
 {
 
-        
+
   WiFiClient client = wifiServer.available();
-          
-  if (client) 
+
+  if (client)
   {
-    if(client.connected()) //Returns true if the client is connected, false if not.
+    if (client.connected()) //Returns true if the client is connected, false if not.
     {
       Serial.println("Client Connected");
     }
-    
-    while(client.connected())
+
+    while (client.connected())
     {
       int len = client.available();  //The number of bytes available.
-      int servoData = 0; 
- 
-      while(len>0)
+      int servoData = 0;
+      int servo3_old = 0;
+      int servo3_new = 0;
+
+      while (len > 0)
       {
         int dataFromClient = client.read(); // read data from the connected client
 
-         if(dataFromClient != -1)
-         {
-            if (servoData == 0)
-              servoData = dataFromClient*256;
-            else
+        if (dataFromClient != -1)
+        {
+          if (servoData == 0)
+            servoData = dataFromClient * 256;
+          else
+          {
+            servoData = servoData + dataFromClient;
+
+            //================================================
+
+            //every interval(1.5) seconds gives the computer drone controll
+            unsigned long currentMillis = millis();
+            if (currentMillis - previousMillis >= interval)
             {
-              servoData = servoData + dataFromClient;
-
-//================================================
-
-              switch(servoData/10000) // seporates the 5 digit integer
-                {
-                 case 1:
-                    servo_1.writeMicroseconds(servoData%10000);
-                  break;
-                 case 2:
-                    servo_2.writeMicroseconds(servoData%10000);
-                 break;
-                 case 3:
-                    servo_3.writeMicroseconds((servoData%10000)-5);
-                 break;
-                 case 4:
-                    servo_4.writeMicroseconds(servoData%10000);
-                 break;
-                 case 5:
-                    servo_5.writeMicroseconds(servoData%10000);
-                 break;
-                }
-
-//================================================
-              servoData = 0;              
+              previousMillis = currentMillis;
+              resetFlags();
             }
-         }
+
+
+            switch (servoData / 10000) // seporates the 5 digit integer
+            {
+              case 1:
+                if (!CH1_Interrupts_Is)
+                  servo_1.writeMicroseconds(servoData % 10000);
+                break;
+              case 2:
+                if (!CH2_Interrupts_Is)
+                  servo_2.writeMicroseconds(servoData % 10000);
+                break;
+              case 3:
+                servo3_new = (servoData % 10000);
+                if (!(servo3_new == servo3_old))
+                {
+                  if (!CH3_Interrupts_Is)
+                  {
+                    servo_3.writeMicroseconds(servo3_new);
+                    servo3_old = servo3_new ;
+                  }
+                }
+                break;
+              case 4:
+                if (!CH4_Interrupts_Is)
+                  servo_4.writeMicroseconds(servoData % 10000);
+                break;
+              case 5:
+                servo_5.writeMicroseconds(servoData % 10000);
+                break;
+            }
+            //================================================
+            servoData = 0;
+          }
+        }
       }
-   }
-   Serial.println("Client Disconnected");
- }
+    }
+    Serial.println("Client Disconnected");
+  }
+}
+
+
+void resetFlags()
+{
+  CH1_Interrupts_Is = false;
+  CH2_Interrupts_Is = false;
+  CH3_Interrupts_Is = false;
+  CH4_Interrupts_Is = false;
+}
+
+
+ICACHE_RAM_ATTR void CH1_INTERRUPT()
+{
+  int channelNew = pulseIn(CH1_Input, HIGH);
+
+  if ( (CH1_Receiver_Old < (channelNew - interuptSensitivity)) || (CH1_Receiver_Old > (channelNew + interuptSensitivity)) )
+  {
+    CH1_Interrupts_Is = true;
+    servo_1.writeMicroseconds(CH1_Receiver_Old);
+    CH1_Receiver_Old = channelNew;
+  }
+}
+
+ICACHE_RAM_ATTR void CH2_INTERRUPT()
+{
+  int channelNew = pulseIn(CH2_Input, HIGH);
+
+  if ( (CH2_Receiver_Old < (channelNew - interuptSensitivity)) || (CH2_Receiver_Old > (channelNew + interuptSensitivity)) )
+  {
+    CH2_Interrupts_Is = true;
+    servo_2.writeMicroseconds(CH2_Receiver_Old);
+    CH2_Receiver_Old = channelNew;
+  }
+}
+
+ICACHE_RAM_ATTR void CH3_INTERRUPT()
+{
+  int channelNew = pulseIn(CH3_Input, HIGH);
+
+  if ( (CH3_Receiver_Old < (channelNew - interuptSensitivity)) || (CH3_Receiver_Old > (channelNew + interuptSensitivity)) )
+  {
+    CH3_Interrupts_Is = true;
+    servo_3.writeMicroseconds(CH3_Receiver_Old);
+    CH3_Receiver_Old = channelNew;
+  }
+}
+
+ICACHE_RAM_ATTR void CH4_INTERRUPT()
+{
+  int channelNew = pulseIn(CH4_Input, HIGH);
+
+  if ( (CH4_Receiver_Old < (channelNew - interuptSensitivity)) || (CH4_Receiver_Old > (channelNew + interuptSensitivity)) )
+  {
+    CH4_Interrupts_Is = true;
+    servo_4.writeMicroseconds(CH4_Receiver_Old);
+    CH4_Receiver_Old = channelNew;
+  }
 }
